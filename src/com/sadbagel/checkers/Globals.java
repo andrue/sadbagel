331ca98141c5252c0ba72c3994307e479ab38733
Andrew Odensos <odensos1@umbc.edu>
2012-05-06 14:37:33 -0400

    Removed some extra files from the git.

--- src/com/sadbagel/checkers/Globals.java
+++
@@ -1,9 +0,0 @@
-package com.sadbagel.checkers;
-
-public class Globals {
-	public static boolean	RESOURCES_INITIATED	= false;
-	public static boolean	restartGame			= false;
-	public static boolean	paused			= false;
-	public static Checkers GAME = null;//This is something I'm going to hell for.
-	public static int CURRENTSTATE;
-}
