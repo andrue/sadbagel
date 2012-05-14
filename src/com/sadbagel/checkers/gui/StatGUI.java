3d3b8119f164873e31be9e10009d7bc526367fa2
Ryan WF <wnukry1@umbc.edu>
2012-05-14 14:25:53 -0400

    the stat

--- src/com/sadbagel/checkers/gui/StatGUI.java
+++ src/com/sadbagel/checkers/gui/StatGUI.java
@@ -1,26 +1,36 @@
 package com.sadbagel.checkers.gui;
 
+import java.awt.Font;
+
 import org.newdawn.slick.Color;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
 import org.newdawn.slick.SlickException;
+import org.newdawn.slick.UnicodeFont;
+import org.newdawn.slick.gui.TextField;
+
+import com.sadbagel.checkers.backend.CheckerStats;
 
 public class StatGUI {
 	
 	private int activated = 0;
 	private Image title = null;
+	private TextField text = null;
 	
 	public StatGUI(){
 		
 	}
 	
 	public void init(){
-		title = ResourceManager.getImage("titleStatistics");
+		title = ResourceManager.getImage("titleStatistics");//TODO: V This is what is wrong with the whole idea. I don't know how to get the text box to pop up. Set text's text field to CheckerStats.text() and we're good. 
+		text = new TextField(Globals.GAME.getContainer(), (org.newdawn.slick.Font) new UnicodeFont(new Font("Times New Roman", 20, 20)), 4, 4, 300, 400);
 	}
 	
 	public void toggle(){
 		activated = (activated + 1) % 2;
+		CheckerStats.refresh();
+		text.setText(CheckerStats.text());
 	}
 	
 	public boolean isActivated(){
@@ -39,6 +49,8 @@ public class StatGUI {
 			g.fillRect(0, 0, 800, 600);
 			
 			title.draw(210, 42);
+
+			text.render(container, g);
 			
 			//Draw the statistics to the screeeeen
 			
